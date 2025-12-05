91bb3f289b4abe2e61d7fa0221b0fbd2bb95854e
memothelemo <dev@memothelemo.xyz>
2025-12-05 16:50:02 +0800

    Allow users to run a command or use interactive mode

--- src/cli/check_tools.rs
+++ src/cli/check_tools.rs
@@ -1,18 +1,21 @@
 use anstream::{eprintln, println};
 use anyhow::Result;
 
+use console::Term;
 use ctftools::install::check_toolkit_installation;
 use ctftools::registry::Toolkit;
 
 use crate::ansi::*;
 
-pub fn run(toolkit: &Toolkit) -> Result<()> {
+pub fn run(term: &Term, toolkit: &Toolkit) -> Result<()> {
     eprintln!("⏳ {BOLD}Checking the installation of all built-in tools...{BOLD:#}");
 
     let results = check_toolkit_installation(toolkit)?;
-    let expected_count = results.len();
+    crate::clear_last_lines(term, 1)?;
 
     let mut installed_count = 0usize;
+    let expected_count = results.len();
+
     for (tool, installed) in results {
         let (emoji, style) = if installed {
             installed_count += 1;
--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,15 +1,18 @@
 use anstyle::Style;
 use anyhow::{Context, Result};
 use clap::Parser;
+use console::Term;
 use log::{debug, info};
 use std::io::Write;
 
 use ctftools::registry::Toolkit;
 
+mod action;
 mod ansi;
 mod check_tools;
 mod options;
 
+use crate::action::Action;
 use crate::ansi::{BOLD, GRAY};
 use crate::options::{Command, Options};
 
@@ -22,30 +25,132 @@ fn main() -> Result<()> {
 
     // Prevent accidental termination via CTRL+C (unless we need to)
     ctrlc::set_handler(|| {}).context("failed to set handler for CTRL+C")?;
-    print_header();
 
     // Load our toolkit to be used for the entire program's lifetime.
     let toolkit = init_maybe_custom_toolkit(&opts)?;
-    match opts.command {
-        Some(Command::Check) => {
-            self::check_tools::run(&toolkit)?;
+    let term = Term::stderr();
+
+    let mut action = match opts.command {
+        Some(Command::Check) => Some(Action::CheckTools),
+        Some(Command::InstallTools) => Some(Action::InstallTools),
+        None => None,
+    };
+
+    loop {
+        print_header();
+
+        let mut interactive_mode = true;
+        let action = match action.take() {
+            // This came from the CLI so yes...
+            Some(action) => {
+                print_header_line();
+                interactive_mode = false;
+                Some(action)
+            }
+            None => {
+                print_header_prompt_instructions();
+                print_header_line();
+                self::action::prompt_select_action(&toolkit)?
+            }
+        };
+        term.show_cursor()?; // try to restore our cursor if CTRL+C has triggered
+
+        // Clear the usage and the extra line so we can try to perform an action?
+        if action.is_some() && interactive_mode {
+            clear_last_lines(&term, 4)?;
         }
-        Some(Command::InstallTools) => todo!(),
-        None => todo!(),
+
+        // If the prompt is interrupted, we can assume that the user
+        // wants to exit the program.
+        let action = action.unwrap_or(Action::Exit);
+        debug!("selected action: {action:?}");
+
+        let result = match action {
+            Action::Tool(metadata) => todo!(),
+            Action::InstallTools => todo!(),
+            Action::InstallAllTools => todo!(),
+            Action::CheckTools => self::check_tools::run(&term, &toolkit),
+            Action::Exit => break,
+        };
+        term.show_cursor()?;
+
+        // If it has an error then, immediately stop the program.
+        if let Err(error) = result {
+            return Err(error).context("error occurred while trying to perform this command");
+        }
+
+        // Then prompt the user if they want to go back to the menu
+        if !interactive_mode || !prompt_repeat()? {
+            break;
+        }
+
+        eprintln!();
     }
 
     Ok(())
 }
 
+fn clear_last_lines(term: &Term, n: usize) -> std::io::Result<()> {
+    // Do not clear last lines if debug logging is enabled.
+    if debug_enabled() {
+        Ok(())
+    } else {
+        term.clear_last_lines(n)
+    }
+}
+
+const CLI_HEADER: &str = concat!("CTF Tool Selector (", env!("CARGO_PKG_REPOSITORY"), ")");
+
+fn prompt_repeat() -> Result<bool> {
+    fn prompt_repeat_inner() -> Result<Option<bool>> {
+        let input: Option<String> = dialoguer::Input::new()
+            .with_prompt("Do you want to select a tool again? [Y/n]")
+            .interact_text()
+            .map(Some)
+            .or_else(|error| match error {
+                dialoguer::Error::IO(inner) if inner.kind() == std::io::ErrorKind::Interrupted => {
+                    Ok(None)
+                }
+                dialoguer::Error::IO(error) => Err(error),
+            })
+            .context("failed to prompt response")?;
+
+        // If it's interrupted, then force exit!
+        let Some(response) = input else {
+            eprintln!();
+            debug!("got interrupted");
+            return Ok(Some(false));
+        };
+
+        match response.chars().next().map(|v| v.to_ascii_lowercase()) {
+            Some('y') => Ok(Some(true)),
+            Some('n') => Ok(Some(false)),
+            _ => Ok(None),
+        }
+    }
+
+    loop {
+        match prompt_repeat_inner()? {
+            Some(true) => return Ok(true),
+            Some(false) => return Ok(false),
+            None => {}
+        };
+    }
+}
+
 fn print_header() {
-    let header = format!("CTF Tool Selector ({})", env!("CARGO_PKG_REPOSITORY"));
-    eprintln!("{BOLD}{header}{BOLD:#}");
+    eprintln!("{BOLD}{CLI_HEADER}{BOLD:#}");
+}
+
+fn print_header_line() {
+    eprintln!("{}", "-".repeat(CLI_HEADER.len() + 2));
+}
+
+fn print_header_prompt_instructions() {
     eprintln!("{GRAY}Choose a tool to see quick usage notes.");
 
     eprint!("Press up or down arrow keys to select a choice");
     eprintln!("{GRAY:#}");
-
-    eprintln!("{}", "-".repeat(header.len() + 2));
 }
 
 fn init_maybe_custom_toolkit(opts: &Options) -> Result<Toolkit> {
